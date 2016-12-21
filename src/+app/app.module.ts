c5124827cefc21a1497410add815980bf14a66b7
Cristian Buffa <bufface@gmail.com>
2016-12-21 17:50:36 -0300

    Toolbar de Material Design

--- src/+app/app.module.ts
+++ src/+app/app.module.ts
@@ -9,11 +9,13 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
 
 
 @NgModule({
   declarations: [ AppComponent ],
   imports: [
+    MaterialModule.forRoot(),
     SharedModule,
     HomeModule,
     AboutModule,
